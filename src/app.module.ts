4111a012ca2249c173a94f452887c1604d4a5014
sujianqingfeng <121116111@qq.com>
2023-10-10 18:14:17 +0800

    wip: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
