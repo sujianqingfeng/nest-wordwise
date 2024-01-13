87c4f52c5800972b1482e388a74655ef70f7847c
hens <121116111@qq.com>
2024-01-13 15:19:04 +0800

    chore: deploy vervel

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
-  const configService = app.get(ConfigService)
-  const port = configService.get('PORT')
-  await app.listen(port)
+  await app.listen(3000)
 }
 
 bootstrap()
